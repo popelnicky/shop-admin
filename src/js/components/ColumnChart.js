974d70027a92ffd55f96bde5e5a1b880e04ededf
Popelnitskiy Dmitriy <popelnitskiyd@arkadium.com>
2020-02-23 14:29:46 +0300

    added ColumnChart component

---
+++ src/js/components/ColumnChart.js
@@ -0,0 +1,27 @@
+class ColumnChart {
+    constructor(data) {
+        this.header = data.header;
+        this.amount = data.amount;
+        this.data = data.values;
+    }
+
+    getBoardHTML() {
+        return this.data.map(item => `<div style="--value:${item}" data-tooltip="${item}"></div>`).join("");
+    }
+
+    render() {
+        return `<div class="column-chart__title">${this.header}</div>
+                <div class="column-chart__container">
+                    <div class="column-chart__header">${this.amount}</div>
+                    <div class="column-chart__chart">
+                        ${this.getBoardHTML()}
+                    </div>
+                </div>`;
+    }
+
+    destroy() {
+        this.header = null;
+        this.amount = null;
+        this.data = null;
+    }
+}
---
+++ src/js/data/common.js
@@ -0,0 +1,15 @@
+const ordersData = {
+    header: "Total orders",
+    amount: "344",
+    values: [28,28,23,7,34,36,39,7,34,42,42,7,31,39,36,18,15,34,34,36,5,34,42,44,5,42,50,34,15,50]
+};
+const salesData = {
+    header: "Total sales",
+    amount: "$243,437",
+    values: [19,12,16,3]
+};
+const customersData = {
+    header: "Total customers",
+    amount: "321",
+    values: [27,30,25,8,36,36,36,8,36,44,44,5,33,33,36,19]
+};
